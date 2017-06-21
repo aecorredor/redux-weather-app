bfaec7cc0ffc086c7f4a19d12cc43a5cdad97f5d
Alejandro Corredor <aecorredor93@gmail.com>
2017-06-21 16:47:15 -0400

    disable host check in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './',
-    watchOptions: { poll: true }
+    watchOptions: { poll: true },
+    disableHostCheck: true,
   }
 };
