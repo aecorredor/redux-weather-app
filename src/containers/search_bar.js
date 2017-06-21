2104b7d964100df705250b64d767cda34b601428
Alejandro Corredor <aecorredor93@gmail.com>
2017-06-21 16:19:33 -0400

    finish adding google maps for each city

---
+++ src/containers/search_bar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  onInputChange(event) {
+    this.setState({
+      term: event.target.value
+    });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // We need to go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={(event) => this.onFormSubmit(event)} className="input-group">
+        <input
+          onChange={(event) => this.onInputChange(event)}
+          value={this.state.term}
+          placeholder="Search for a city's five day weather forecast"
+          className="form-control"
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: './',
+    watchOptions: { poll: true }
   }
 };
